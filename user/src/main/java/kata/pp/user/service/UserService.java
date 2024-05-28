7ae7bfea27b49691bfa8c34e0a93668f8d077a98
Анатолий Лосев <kleshyk@gmail.com>
2024-05-29 01:12:58 +0300

    исправил ошибки

--- user/src/main/java/kata/pp/user/service/UserService.java
+++ user/src/main/java/kata/pp/user/service/UserService.java
@@ -1,40 +1,19 @@
 package kata.pp.user.service;
 
 import kata.pp.user.model.User;
-import kata.pp.user.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class UserService {
+public interface UserService {
 
-    private final UserRepository userRepository;
+    public List<User> getAllUsers();
 
-    @Autowired
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
+    public User getUserById(Long id);
 
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
-    public User getUserById(Long id) {
-        return userRepository.findById(id).orElse(null);
-    }
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-    public void deleteUser(Long id) {
-        userRepository.deleteById(id);
-    }
-    public void updateUser(User user) {
-        userRepository.save(user);
-    }
+    public User saveUser(User user);
 
+    public void updateUser(User user);
 
-
+    public void deleteUser(Long id);
 
 }
-
